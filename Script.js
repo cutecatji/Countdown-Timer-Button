211c20309b54fab1141f3c2f16b838063c8d8db0
cutecatji <163742860+cutecatji@users.noreply.github.com>
2024-08-20 17:37:09 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,18 @@
+let CountdownTimer;//Hold setInterval ID
+let btn=document.querySelector("button");
+btn.onclick=()=>{
+    if(CountdownTimer)
+        clearInterval(CountdownTimer);
+    let timeleft=prompt("Enter Time in Seconds:");//set initial value
+    document.querySelector("button").textContent=timeleft;
+    CountdownTimer=setInterval(()=>{
+        timeleft--;
+        document.querySelector("button").textContent=timeleft;
+        if(timeleft<0)
+        {
+            document.querySelector("button").textContent="Time's up!";
+            clearInterval(CountdownTimer);
+        }
+    },1000)
+
+}
